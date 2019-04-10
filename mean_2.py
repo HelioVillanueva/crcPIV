369e4f9e3b148b638eba4dfe52446518f6d86e1e
helio <helio@localhost.localdomain>
2019-04-10 18:50:38 -0300

    executavel utiliza outro executavel. Modulos executaveis independentes facilitam uso apenas do necessario no lugar de unico script para todo o processamento

---
+++ mean_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+===============================================================================
+                      Python code for PIV analysis
+   Created by Combustion Research Center CRC at LETE - Sao Paulo, Brasil
+   Laboratory of Environmental and Thermal Engineering - LETE
+   Escola Politecnica da USP - EPUSP
+   
+===============================================================================
+version:0.0 - 02/2019: Helio Villanueva
+version:1.0 - 04/2019: Helio Villanueva
+"""
+
+import readRaw_1
+
+print (readRaw_1.U.max())
